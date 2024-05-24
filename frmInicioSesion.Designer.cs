a611a823c50684dad74b651cc276e1c272ab9b05
Alumno <Alumno@PC4>
2024-05-24 13:23:17 -0300

    Agregar archivos de proyecto.

---
+++ frmInicioSesion.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PrySignorile_TP2
+{
+    partial class frmInicioSesion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUsuario = new Label();
+            txtUsuario = new TextBox();
+            btnIniciar = new Button();
+            txtContra = new TextBox();
+            lblContra = new Label();
+            btnRegistrar = new Button();
+            SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(29, 20);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(50, 15);
+            lblUsuario.TabIndex = 0;
+            lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(101, 17);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.TabIndex = 1;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Location = new Point(29, 108);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(75, 23);
+            btnIniciar.TabIndex = 2;
+            btnIniciar.Text = "Iniciar";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // txtContra
+            // 
+            txtContra.Location = new Point(101, 63);
+            txtContra.Name = "txtContra";
+            txtContra.PasswordChar = '*';
+            txtContra.Size = new Size(100, 23);
+            txtContra.TabIndex = 4;
+            // 
+            // lblContra
+            // 
+            lblContra.AutoSize = true;
+            lblContra.Location = new Point(29, 66);
+            lblContra.Name = "lblContra";
+            lblContra.Size = new Size(70, 15);
+            lblContra.TabIndex = 3;
+            lblContra.Text = "Contraseña:";
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(127, 108);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(75, 23);
+            btnRegistrar.TabIndex = 5;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
+            // frmInicioSesion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(238, 143);
+            Controls.Add(btnRegistrar);
+            Controls.Add(txtContra);
+            Controls.Add(lblContra);
+            Controls.Add(btnIniciar);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblUsuario);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmInicioSesion";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Inicio Sesion";
+            Load += frmInicioSesion_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUsuario;
+        private TextBox txtUsuario;
+        private Button btnIniciar;
+        private TextBox txtContra;
+        private Label lblContra;
+        private Button btnRegistrar;
+    }
+}
---
+++ frmInicioSesion.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrySignorile_TP2
+{
+    public partial class frmInicioSesion : Form
+    {
+        public frmInicioSesion()
+        {
+            InitializeComponent();
+        }
+        private void RegistrarUsuario()
+        {
+            string nombre = txtUsuario.Text;
+            string contrasena = txtContra.Text;
+
+
+            string datosUsuario = $"{nombre},{contrasena}";
+
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("usuarios.txt", true))
+                {
+                    writer.WriteLine(datosUsuario);
+                    MessageBox.Show("Usuario registrado exitosamente!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar el usuario: " + ex.Message);
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            RegistrarUsuario();
+        }
+
+        private void IniciarSesion()
+        {
+
+            string nombreUsuario = txtUsuario.Text;
+            string contrasena = txtContra.Text;
+
+
+            List<string> usuarios = LeerUsuarios();
+
+
+            foreach (string usuario in usuarios)
+            {
+                string[] datosUsuario = usuario.Split(',');
+                if (datosUsuario[0] == nombreUsuario && datosUsuario[2] == contrasena)
+                {
+                    MessageBox.Show("¡Inicio de sesión exitoso!");
+                    return;
+                }
+            }
+
+
+            MessageBox.Show("Nombre de usuario o contraseña incorrectos.");
+        }
+
+        private List<string> LeerUsuarios()
+        {
+            List<string> usuarios = new List<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader("usuarios.txt"))
+                {
+                    string linea;
+                    while ((linea = reader.ReadLine()) != null)
+                    {
+                        usuarios.Add(linea);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer el archivo de usuarios: " + ex.Message);
+            }
+            return usuarios;
+        }
+        private void frmInicioSesion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            IniciarSesion();
+        }
+    }
+}
